5565b30eedcce7c9f66ee3a2f61ca8d94aa3d89e
pizzahuhthp9 <57580794+pizzahuhthp9@users.noreply.github.com>
2021-01-31 17:14:58 +0700

    config css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,70 @@
+const colors = require("tailwindcss/colors");
+
 module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      black: colors.black,
+      white: colors.white,
+      gray: colors.trueGray,
+      indigo: colors.indigo,
+      red: colors.rose,
+      yellow: colors.amber,
+      orange: {
+        DEFAULT: "#ff6600",
+        light: "#ff8b3d",
+      },
+    },
+    extend: {
+      margin: {
+        55: "13.75rem",
+      },
+      spacing: {
+        18: "4.5rem",
+        22: "5.5rem",
+        38: "9.5rem",
+        68: "17rem",
+      },
+    },
+    opacity: {
+      0: "0",
+      10: ".1",
+      20: ".2",
+      30: ".3",
+      40: ".4",
+      50: ".5",
+      60: ".6",
+      70: ".7",
+      80: ".8",
+      90: ".9",
+      96: ".96",
+      97: ".97",
+      98: ".98",
+      99: ".99",
+      100: "1",
+    },
+    fontSize: {
+      xxs:'.625rem',
+      xs: ".75rem",
+      sm: ".875rem",
+      tiny: ".875rem",
+      base: "1rem",
+      lg: "1.125rem",
+      xl: "1.25rem",
+      "2xl": "1.5rem",
+      "3xl": "1.875rem",
+      "4xl": "2.25rem",
+      "5xl": "3rem",
+      "6xl": "4rem",
+      "7xl": "5rem",
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
