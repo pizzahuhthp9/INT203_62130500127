dd147dbb448b20403fe101fab8e785dc2bf05b4f
pizzahuhthp9 <57580794+pizzahuhthp9@users.noreply.github.com>
2021-01-31 17:14:51 +0700

    add main and test path

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,20 @@
 import {createRouter, createMemoryHistory} from 'vue-router'
 
 import page from '../view/page.vue'
+import main from '../view/main.vue'
+import test from '../view/test.vue'
 
 const routes=[
     {
         path:'/page',
         component:page
+    },
+    {
+        path:'/',
+        component: main
+    },{
+        path:'/test',
+        component: test
     }
 ]
 
