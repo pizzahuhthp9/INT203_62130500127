2d35806f0f6cf93f76c8c404d3ab53b73a875c0f
pizzahuhthp9 <57580794+pizzahuhthp9@users.noreply.github.com>
2021-01-31 17:23:10 +0700

    move project to groupwork_1

---
+++ 62130500127_groupwork_1/src/components/elseButton.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="else-button" class="flex flex-col">
+      <div class="circle w-1 h-1 rounded-full bg-gray-500"></div>
+      <div class="circle w-1 h-1 rounded-full bg-gray-500"></div>
+      <div class="circle w-1 h-1 rounded-full bg-gray-500"></div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
